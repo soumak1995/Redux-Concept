f2bc600e15d4a92dac1f9f4e1e91c90b49eae0e3
unknown <Soumak Maji>
2020-10-19 10:52:42 +0530

    mapStateToProps mapDispacthToProps

--- react-redux-concept/src/App.js
+++ react-redux-concept/src/App.js
@@ -5,11 +5,14 @@ import CakeContainer from './component/CakeContainer'
 import HooksCakeContainer from './component/HooksCakeContainer'
 import IceCreamContainer from './component/IceCreamContainer'
 import {Provider} from 'react-redux'
+import ItemContainer from './component/ItemContainer'
 import store from './Redux/Store'
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer/>
         <HooksCakeContainer/>
         <CakeContainer/>
         <IceCreamContainer/>
